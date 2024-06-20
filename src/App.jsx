9a7fecc64cf34201a251b63d2a65bc2173861c41
Omar Cruz <omar.cruzr97@hotmail.com>
2024-06-19 23:42:33 -0600

    updating

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import './App.css';
-import Cargando from './components/Cargando';
 import Inicio from './components/Inicio'
 import AboutUs from './components/AboutUs'
 import Mapa from './components/Mapa'
@@ -9,27 +8,16 @@ import Error from './components/Error'
 import { BrowserRouter as Router, Routes, Route, Navigate  } from 'react-router-dom';
 
 function App() {
-  const [isLoading, setIsLoading] = useState(true);
-
-  setTimeout(() => {
-    setIsLoading(false);
-  }, 750);
   return (
     <div className="App">
       <Router>
-        {isLoading ? (
-        <Cargando imagen="re2.webp" />
-      ):(
-        <>
-          <Routes>
-            <Route path="/" element={<Inicio/>}/>
-            <Route path="/info" element={<AboutUs/>}/>
-            <Route path="/mapa" element={<Mapa/>}/>
-            <Route path="*" element={<><Error/><Navigate to="/no-encontrado" replace /></>}/>
-          </Routes>
-          <Contacto/>
-        </>
-        )}
+        <Routes>
+          <Route path="/" element={<Inicio/>}/>
+          <Route path="/info" element={<AboutUs/>}/>
+          <Route path="/mapa" element={<Mapa/>}/>
+          <Route path="*" element={<><Error/><Navigate to="/no-encontrado" replace /></>}/>
+        </Routes>
+        <Contacto/>
       </Router>
     </div>
   );
